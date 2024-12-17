84ccd220ec3081b63a900a32a526f5b0b3069b17
ahka07001 <ahka07001@E-PW05CGQT>
2024-12-17 14:07:31 +0100

    Add project files.

---
+++ inlämningsUppgift nivå 1 10/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace inlämningsUppgift_._nivå1_._10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int antalJämna = 0; 
+
+            
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("Skriv in ett heltal: ");
+                int tal = int.Parse(Console.ReadLine()); 
+
+               
+                if (tal % 2 == 0)
+                {
+                    antalJämna++; 
+                }
+            }
+
+            
+            Console.WriteLine("Antalet jämna tal är: " + antalJämna);
+        }
+    }
+}
